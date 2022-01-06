92a61c7d61c1c582d15e211306f37c585f89cd8b
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2022-01-06 21:15:50 +0700

    first section of topic 2

---
+++ Topic02.SimpleManipulationsNumbersAndVectors/01.VectorsAndAssignment.R
@@ -0,0 +1,11 @@
+x <- c(10.4, 5.6, 3.1, 6.4, 21.7); x #create a vector x from given numbers use c() function
+
+#assignment can also be used by the function assign()
+assign("x", c(10.4, 5.6, 3.1, 6.4, 21.7)); x
+
+#the operator <- can also be used in other direction
+c(10.4, 5.6, 3.1, 6.4, 21.7) -> x; x
+
+1/x
+
+y <- c(x, 0, x); y
