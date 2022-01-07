0594293fdeb1c66542e1be1526050e170f717c4d
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2022-01-07 21:56:09 +0700

    topic 2.2

---
+++ Topic02.SimpleManipulationsNumbersAndVectors/02.VectorArithmetic.R
@@ -0,0 +1,18 @@
+x <- c(10.4, 5.6, 3.1, 6.4, 21.7)
+y <- c(x, x)
+v <- 2 * x + y + 1; v #Vectors can be used in arithmetic expressions
+
+min(x)
+max(x)
+range(x)
+length(x)
+sum(x)
+prod(x)
+
+mean(x)
+var(x)
+
+sort(x)
+
+sqrt(-17)
+sqrt(-17 + 0i)
