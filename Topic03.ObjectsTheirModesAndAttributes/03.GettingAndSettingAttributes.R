b17633bc44ec80571cb613f062c9d161477dd5f0
vanloc1808 <76842571+vanloc1808@users.noreply.github.com>
2022-01-09 18:35:11 +0700

    topic 3.3 & topic 3.4

---
+++ Topic03.ObjectsTheirModesAndAttributes/03.GettingAndSettingAttributes.R
@@ -0,0 +1,4 @@
+z <- 0 : 99
+attributes(z)
+attr(z, "dim") <- c(10, 10); 
+attr(z, "dim")
---
+++ Topic03.ObjectsTheirModesAndAttributes/04.TheClassOfAnObject.R
@@ -0,0 +1,5 @@
+#all objects in R have a class, reported by the function class
+#allow for an object-oriented style of programming in R
+#for example, if an object has class "data.frame", it will be printed in a certain way,
+#the plot() function will display it graphically in a certain way,
+#other so-called generic functions such as summary() will react to it as an argument in a way sensitive to its class
